d184fcea6786af9e0e15de209ac48991b0cb1a11
senatrius <g.makutenas@gmail.com>
2023-02-07 15:29:32 +0200

    add location and date components

---
+++ components/Card.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { Credit } from './Credit';
+import { Date } from './Date';
+import { Location } from './Location';
+import { SearchInput } from './SearchInput';
+
+export const Card = () => {
+  return (
+    <div className='relative my-[3%] w-[94%] max-w-[80rem] rounded-2xl border border-cardBorder bg-gradient-to-b from-cardStart to-cardEnd p-6 backdrop-blur-md md:w-[89%] md:py-8 md:px-10'>
+      <Credit />
+      <SearchInput />
+      <div className='mt-6 flex flex-col items-start justify-between gap-4 sm:flex-row md:mt-10'>
+        <Location />
+        <Date />
+      </div>
+    </div>
+  );
+};
---
+++ components/Date.tsx
@@ -0,0 +1,11 @@
+export const Date = () => {
+  return (
+    <div className='sm:text-right'>
+      <p className='text-[1.75rem] leading-none'>
+        00:00
+        <span className='text-xl'>PM</span>
+      </p>
+      <p className='text-lg text-subtext'>15th October, Monday</p>
+    </div>
+  );
+};
