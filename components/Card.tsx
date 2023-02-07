af6122cfb4cdf6affdbc200b9900f3275788ab4c
senatrius <g.makutenas@gmail.com>
2023-02-07 16:12:12 +0200

    add heading tags for accessibility

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,13 +9,16 @@ import { SearchInput } from './SearchInput';
 export const Card = () => {
   return (
     <div className='relative my-[3%] w-[94%] max-w-[80rem] rounded-2xl border border-cardBorder bg-gradient-to-b from-cardStart to-cardEnd p-6 backdrop-blur-md md:w-[89%] md:py-8 md:px-10'>
+      <h1 className='sr-only'>Air quality index application</h1>
       <Credit />
       <SearchInput />
       <div className='mt-6 flex flex-col items-start justify-between gap-4 sm:flex-row md:mt-10'>
+        <h2 className='sr-only'>Date and location info</h2>
         <Location />
         <Date />
       </div>
       <div className='mt-10 flex flex-col items-start justify-between gap-4 sm:flex-row md:mt-16'>
+        <h2 className='sr-only'>Air quality data</h2>
         <AQI />
         <Data />
       </div>
