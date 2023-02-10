157f5c4a65660a0a5a43366521c8dbdfa3f5dc36
senatrius <g.makutenas@gmail.com>
2023-02-10 22:47:34 +0200

    fix build error

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -38,28 +38,25 @@ export const SearchInput = ({
         onKeyUp={onKeyUp}
         value={searchQuery}
       />
-      {suggestions &&
-        suggestions.status === 'ok' &&
-        suggestions !== {} &&
-        isSuggestionOpen && (
-          <div className='absolute mt-4 max-h-[12rem] w-full overflow-y-auto rounded-lg text-black'>
-            {suggestions.data?.map(
-              (suggestion: ISuggestion, idx: number) =>
-                suggestion.aqi !== '-' && (
-                  <button
-                    data-id={suggestion.uid}
-                    onMouseDown={search}
-                    className='w-full bg-white py-3 px-5 text-left hover:bg-subtext focus:bg-subtext'
-                    key={idx}>
-                    <span className='mr-4 text-lg font-medium'>
-                      {suggestion.aqi}
-                    </span>
-                    {suggestion.station.name}
-                  </button>
-                )
-            )}
-          </div>
-        )}
+      {suggestions && suggestions.status === 'ok' && isSuggestionOpen && (
+        <div className='absolute mt-4 max-h-[12rem] w-full overflow-y-auto rounded-lg text-black'>
+          {suggestions.data?.map(
+            (suggestion: ISuggestion, idx: number) =>
+              suggestion.aqi !== '-' && (
+                <button
+                  data-id={suggestion.uid}
+                  onMouseDown={search}
+                  className='w-full bg-white py-3 px-5 text-left hover:bg-subtext focus:bg-subtext'
+                  key={idx}>
+                  <span className='mr-4 text-lg font-medium'>
+                    {suggestion.aqi}
+                  </span>
+                  {suggestion.station.name}
+                </button>
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 };
